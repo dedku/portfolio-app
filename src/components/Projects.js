1370d1e8e892d4cb11cb6e39bf9f42812e87d204
Patryk Szabat <szabat.patryk@gmail..com>
2022-06-23 17:56:26 +0200

    project content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,12 @@
-import { Container, SimpleGrid, Box, Text, Flex, Spacer, VStack } from "@chakra-ui/react";
+import { Container, SimpleGrid, Box, Text, Flex, Spacer, VStack, Button, Divider, Badge, Center } from "@chakra-ui/react";
 import { RiVuejsFill } from 'react-icons/ri'
 import { IoLogoJavascript } from 'react-icons/io'
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { IoLogoGithub } from 'react-icons/io'
+import { CgWebsite } from 'react-icons/cg'
+
+
 
 const Projects = () => {
     const [ isHovered, setIsHovered ] = useState(false)
@@ -28,7 +32,7 @@ const Projects = () => {
         >
             <Box role='group'>
                 <Box pos="relative" h={{ base: 'calc(100vh - 12rem)', md: 'calc(50vh - 1rem)' }} rounded='xl' position='relative' onMouseOver={over} onMouseOut={out}>
-                    <Box bgImage="url('/Poligrafia-łowicz.png')" bgSize='cover' rounded='xl' position="absolute" top='0' right='0' left='0' bottom='0'  zIndex='-2' opacity='0.85' transition='0.5s' _groupHover={{ opacity: '0.1', transition: '0.3s'}}/>
+                    <Box bgImage="url('/Poligrafia-łowicz.png')" bgSize='cover' rounded='xl' position="absolute" top='0' right='0' left='0' bottom='0'  zIndex='-2' opacity='0.85' transition='0.5s' _groupHover={{ opacity: '0.05', transition: '0.3s'}}/>
                     <Flex>
                         <Text color="white" m={4} p={2} rounded='xl' bg="blackAlpha.900" fontSize={14} fontFamily='JetBrains Mono  , Poppins' fontWeight='bold' letterSpacing='wide'>Poligrafia Łowicz</Text>
                         <Spacer />
@@ -40,11 +44,34 @@ const Projects = () => {
                         </Box>
                     </Flex>
                     {isHovered &&
-                    <VStack>
-                    <motion.div   initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.1 }}><Text >test1</Text></motion.div>
-                    <motion.div   initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.3 }}><Text >test2</Text></motion.div>
-                    <motion.div   initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.6 }}><Box >test3    </Box></motion.div>
-                    </VStack>}
+                    <Center>
+                    <VStack gap={8}>
+                        <motion.div   initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.05 }} >
+                            <Box display='flex' justifyContent='space-between' w={64}>
+                                <Button bg='blackAlpha.900' color='white'>
+                                    <IoLogoGithub fontSize={64}/>
+                                </Button>
+                                <Button bg='blackAlpha.900' color='white'>
+                                    <CgWebsite fontSize={64}/>
+                                </Button>
+                            </Box>
+                        </motion.div>
+                        <motion.div   initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.3 }} w='100%'>
+                            <Badge colorScheme='yellow' rounded='xl' p={2} mb={4}>In progress 90%</Badge>
+                            <Divider w={64}/>
+                        </motion.div>
+                        <motion.div   initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.6 }} mx='auto'>
+                        <Text mx={14} > Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse auctor enim ac ullamcorper condimentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer non dui sollicitudin, sagittis arcu ut, euismod mauris. In hendrerit velit vel felis porttitor mollis. </Text>
+                        </motion.div>
+                        <motion.div   initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.9 }}>
+                            <Box display='flex' justifyContent='space-between' w={64} >
+                                <Badge colorScheme='green' rounded='xl' p={2}>Vue 3</Badge>
+                                <Badge colorScheme='green' rounded='xl' p={2}>Vuex</Badge>
+                                <Badge colorScheme='yellow' rounded='xl' p={2}>JavaScript</Badge>
+                            </Box>
+                        </motion.div>
+                    </VStack>
+                    </Center>}
                 </Box>
             </Box>
             <Box bg="papayawhip" h={{ base: 'calc(100vh - 12rem)', md: 'calc(50vh - 1rem)' }} borderRadius={4}  />
