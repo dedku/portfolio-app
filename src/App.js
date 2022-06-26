5d460ebde3fc55f18f2d23d5d3b8b37b25b86601
dedku <szabat.patryk@gmail.com>
2022-06-26 22:17:57 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Fonts from './components/Fonts';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <AboutMe />
       <Projects />
+      <Footer />
     </ChakraProvider>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import {
+    Box,
+    Center,
+    Container,
+    Stack,
+    Text,
+    useColorModeValue,
+  } from '@chakra-ui/react';
+
+
+
+  export default function SmallWithLogoLeft() {
+    return (
+      <Box
+        bg={useColorModeValue('blackAlpha.300', '#20202380')}
+        color={useColorModeValue('gray.700', 'gray.200')}>
+        <Container
+          as={Stack}
+          maxW={'6xl'}
+          py={4}
+          direction={{ base: 'column', md: 'row' }}
+          justify='center'
+          align={{ base: 'center', md: 'center' }}>
+          <Center>© 2022 Patryk Szabat. All rights reserved</Center>
+        </Container>
+      </Box>
+    );
+  }
